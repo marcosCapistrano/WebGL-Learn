3fac5bdce7bf28b1d726f3242089b91f43fb249f
Marcos Capistrano <mcapistrano50@gmail.com>
2021-05-13 14:44:03 -0300

    Multiple semi-circles being drawn via fragment shaders!

--- learn/index.js
+++ learn/index.js
@@ -1,48 +1,28 @@
 const gl = document.getElementById("c").getContext("webgl");
 const programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
- 
-  var arrays1 = {
-    a_pos: [ 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, -1.0 ],
+
+  var arrays = {
+    a_pos: [0, 0, 0, 0.5, 0.5, 0, -0.5, 0.5, 0],
   };
 
-
-  var buffers = [
-    twgl.createBufferInfoFromArrays(gl, arrays1),
-  ]
+  const buffer = twgl.createBufferInfoFromArrays(gl, arrays)
  
   function render(time) {
     twgl.resizeCanvasToDisplaySize(gl.canvas);
-    gl.viewport( 0, 0, gl.canvas.width, gl.canvas.height );
-    gl.enable( gl.DEPTH_TEST );
-   // gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-    //gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
     const uniforms = {
+      time: time * 0.001,
       u_res: [gl.canvas.width, gl.canvas.height],
-      aRadius: 500,
-      aColor: [1.0, 1.0, 0.0, 1.0] 
-    }
+    };
  
     gl.useProgram(programInfo.program);
 
-    for(buffer of buffers) {
     twgl.setBuffersAndAttributes(gl, programInfo, buffer);
     twgl.setUniforms(programInfo, uniforms);
     twgl.drawBufferInfo(gl, buffer, gl.POINTS);
-    }
  
     requestAnimationFrame(render);
   }
 
-  function update() {
-      const text = document.querySelector("#array");
-      let array = text.value.split(", ");
-      array = {
-          a_pos: array
-      }
-
-
-      console.log(array)
-      bufferInfo = twgl.createBufferInfoFromArrays(gl, array);
-      console.log(bufferInfo)
-  }
   requestAnimationFrame(render);
